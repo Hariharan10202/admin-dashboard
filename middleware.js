3ed846f72c421420b412fbaf2c9746b142c14349
Hariharan <hariharan10202@gmail.com>
2024-03-01 19:17:56 +0530

    middleware changed

--- middleware.js
+++ middleware.js
@@ -1,16 +1,26 @@
 import { withAuth } from "next-auth/middleware";
+import { publicRoutes } from "./routes";
+import { NextResponse } from "next/server";
 
 export default withAuth(function middleware(req) {}, {
   callbacks: {
     authorized: ({ req, token }) => {
-      if (req.nextUrl.pathname.startsWith("/dashboard") && token === null) {
-        return false;
-      } else if (req.nextUrl.pathname === "/dashboard" && token === null) {
-        return false;
-      } else if (req.nextUrl.pathname === "/" && token) {
+      const { nextUrl } = req;
+      const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+      if (isPublicRoute) {
+        if (token) {
+          return NextResponse.redirect(new URL("/dashborad", nextUrl));
+        }
         return true;
+      }
+      if (!isPublicRoute && !token) {
+        return Response.redirect(new URL("/signin", req.url));
       }
       return true;
     },
   },
 });
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
